dbe4219cf8b029554e5f5da177c8a728deba5411
urianlinck <urianlinck@gmail.com>
2023-05-05 15:00:53 -0300

    pronto

---
+++ template/src/Componentes/Garagem/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export const Botao = styled.button `
+
+    background-color: coral;
+    border: 1px solid black;
+    padding: 5px;
+    color: black;
+
+`
+
+export const GaragemContainer = styled.main `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
+export const Estacionamento = styled.section `
+    gap: 5%;
+    display: flex;
+`
